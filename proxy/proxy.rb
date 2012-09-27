0ad01a1c0f3ee414b057e2890641d3d279db3baa
Paul Scarrone <paul.scarrone@gmail.com>
2012-09-27 14:09:45 -0400

    added generic ipad useragent

--- proxy/proxy.rb
+++ proxy/proxy.rb
@@ -5,7 +5,9 @@ require 'uri'
 class Proxy
   attr_accessor :agents
   def initialize()
-    @agents = {"iphone4"=>"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5"}
+    @agents = {
+      "iphone4"=>"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5",
+      "ipad"=>"Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10"}
   end
   
 =begin
