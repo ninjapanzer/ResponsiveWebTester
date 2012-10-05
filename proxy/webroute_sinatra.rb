ab95dd946b529468fcef80aaba6985d86e049b4a
Paul Scarrone <paul.scarrone@gmail.com>
2012-10-04 20:06:52 -0400

    added proxy method to sinatra

--- proxy/webroute_sinatra.rb
+++ proxy/webroute_sinatra.rb
@@ -11,3 +11,14 @@ get '/fetch/:device' do |device|
   p.fetch(passin)
   #"Hello, #{name}. #{params.inspect}"
 end
+
+get '/proxy/:device' do |device|
+  p = Proxy.new
+  passin = {
+    :uri=>params['url'],
+    :headers=>{"HTTP_USER_AGENT"=>p.agents[device]}
+  }
+  #passin[:headers]["HTTP_USER_AGENT"]
+  p.proxy_it(passin)
+  #"Hello, #{name}. #{params.inspect}"
+end
