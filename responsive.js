7a082d2f3375f0acb2a8b6355233e0f809d51b2b
Paul Scarrone <paul.scarrone@gmail.com>
2012-10-30 11:40:27 -0400

    Design Tweaks and Change from nu.nurelm.com as the default site to nurelm.com

--- responsive.js
+++ responsive.js
@@ -1,6 +1,6 @@
 //CONFIG
 //Base URL
-var url = "http://nu.nurelm.com";
+var url = "http://nurelm.com";
 //UserAgent String for Testing
 var IPHONE ="Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5";
 
